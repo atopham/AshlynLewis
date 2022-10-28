cbfe28ccf6b763690b8b0297a032acb981e9fb01
EVELYN-RYAN <72516405+EVELYN-RYAN@users.noreply.github.com>
2022-10-27 23:58:30 -0600

    Venv activate aid

--- ashlynlewis/manage.py
+++ ashlynlewis/manage.py
@@ -13,7 +13,7 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "forget to activate a virtual environment? cmd Windows - ./venv/Scripts/activate.bat  Mac - source ./venv/bin/activate"
         ) from exc
     execute_from_command_line(sys.argv)
 
